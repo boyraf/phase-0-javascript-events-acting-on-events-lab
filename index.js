ede7c123d49091b32b71b79e89d65cb175c791bc
boyraf <raphael.kymotho@gmail.com>
2023-03-26 23:12:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left);
+  if (left < 380) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
